9db6bc957cd9bc6bde91d22c9c729f49a1e68d86
Gabriel Adorno <89091348+cuca2424@users.noreply.github.com>
2025-02-03 12:25:53 -0300

    Update Login.js

--- client/src/paginas/Auth/Login.js
+++ client/src/paginas/Auth/Login.js
@@ -17,7 +17,7 @@ function Login() {
     }
 
     try {
-      const resposta = await fetch("https://gerenciamento-cripto.onrender.com/login", {
+      const resposta = await fetch("http://3.14.128.97:3000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
